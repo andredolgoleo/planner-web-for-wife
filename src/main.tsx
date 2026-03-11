61961a8a6a450c185cf65563112f12bc49f2ef0b
Andrey Dolhov <andrej.dolgov.042110@gmail.com>
2026-03-11 23:52:28 +0100

    chore: moved router into app

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { RouterProvider, createRouter } from "@tanstack/react-router";
-import { routeTree } from "../routeTree.gen";
-
-const router = createRouter({
-  routeTree,
-  defaultPreload: "intent",
-  scrollRestoration: true,
-});
-
-declare module "@tanstack/react-router" {
-  interface Register {
-    router: typeof router;
-  }
-}
+import { App } from "./App";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </StrictMode>,
 );
